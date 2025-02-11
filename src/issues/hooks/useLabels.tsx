d013b9b81f1cf567c58142ee516b6bee3ef45d28
Hyatus <mejia.cristian583@gmail.com>
2025-02-10 22:16:45 -0600

    FIN SECCION 3

---
+++ src/issues/hooks/useLabels.tsx
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import { getLabels } from "../actions";
+
+export const useLabels = () => {
+  const labelsQuery = useQuery({
+    queryKey: ["labels"],
+    queryFn: getLabels,
+    staleTime: 1000 * 60 * 60, // 1 hora de stale time - query fresh B)
+    placeholderData: [
+      {
+        id: 791921801,
+        node_id: "MDU6TGFiZWw3OTE5MjE4MDE=",
+        url: "https://api.github.com/repos/facebook/react/labels/%E2%9D%A4%EF%B8%8F",
+        name: "❤️",
+        color: "ffffff",
+        default: false,
+      },
+    ],
+  });
+
+  return {
+    labelsQuery,
+  };
+};
